6c9a37898d5c68c8f1a1b667686f6d50a93054b7
rokinfuse <rokin@fusemachines.com>
2018-12-26 11:45:39 +0545

    Spring demo project

---
+++ src/main/java/com/rokin/spring/SpringGitApplication.java
@@ -0,0 +1,14 @@
+package com.rokin.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringGitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringGitApplication.class, args);
+	}
+
+}
+
